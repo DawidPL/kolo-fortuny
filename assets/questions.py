d2e6e6be232bb22bb67ee6dfbc94aaaf9e3ee65e
DawidPL <32899009+DawidPL@users.noreply.github.com>
2019-01-28 18:30:43 +0100

    Update questions.py

--- assets/questions.py
+++ assets/questions.py
@@ -3,7 +3,6 @@ words ={'film':['CZTEREJ PANCERNI I PIES', 'KARMAZYNOWY PRZYPŁYW', 'CZTERY WESE
         'geografia': ['PUSZCZA BIAŁOWIESKA', 'GÓRY STOŁOWE', 'KORDYLIERY', 'OCEAN ATLANTYCKI', 'KOŁO PODBIEGUNOWE POŁUDNIOWE'],
         'muzyka': ['TINA TURNER', 'THE BEATLES', 'METALLICA']}
 
-
 class Titles:
     def __init__(self, random_word, word = ''):
         self.random_word = random_word
@@ -11,11 +10,13 @@ class Titles:
     def word_shuffle(self):
         self.random_word = random.choice(list(words))
         return 'Kategoria: ' + self.random_word
+    #def testowa(self):
+        #self.word = random.choice(words[self.random_word])
+       # return self.word
     def word_pick(self):
         self.word = random.choice(words[self.random_word])
-        self.word.replace(word, '*****')
-        return self.word
+        self.word = (i.replace(i, '*') if i != ' ' else i.replace(i, ' ') for i in self.word )
+        for w in self.word:
+            print (*w, end='')
 
 title = Titles(words)
-
-
