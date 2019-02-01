1243c06f050825e10bb96b75b46de6d4a580bfc9
DawidPL <32899009+DawidPL@users.noreply.github.com>
2019-02-01 18:22:26 +0100

    Delete circle.py

--- circle.py
+++
@@ -1,6 +0,0 @@
-import random
-possible_field = {'BANKRUT':0, '-50%': -50, '100': 100, '200': 200, '300':300, '500':500, '1000':1000, '1500':1500, '2000':2000}
-probability_list = [random.choice(list(possible_field)) for i in range(25)]
-def user_point_field():
-    user_field = random.choice(probability_list)
-    return user_field
